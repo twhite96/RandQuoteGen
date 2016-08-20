0bd164ab59b70abafe5680dc932aa91d1fbfd6a6
Tiffany White <tiffanywhitedev412@gmail.com>
2016-08-19 22:01:37 -0400

    sources still not updating. Fixed, if not slow

--- js/app.js
+++ js/app.js
@@ -1,36 +1,39 @@
 $(document).ready(function() {
-   $('#new-quote').on('touchstart click', function(e){
-     e.preventDefault();
-	 var url = 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=';
-     $quoteBox = $('.js-quoteBox');
-     // Give the paragraph for quotes a class name to be precise
-     // I chose to give it .js-quoteBox
-     // Hide it in the DOM (You should do this in CSS instead)
-     $quoteBox.hide();
-     // Make the request
-     $.ajax({
-       url: url,
-       method: 'get',
-       cache: false
-     }).done(function(res){
-       // Use response here to modify the DOM
-       var post = res.shift();
-       $quoteBox.html(post.content);
-       // Now you can fade the js-quoteBox back into the DOM
-       $quoteBox.fadeToggle('slow', function(){
-         $(this).html(post.content);
-         // Show source if available
-         if(post.custom_meta > 0 && post.custom_meta.Source.length > 0){
-           $('#quote-source').html('Source: ' + post.custom_meta.Source);
-         }
-       });
+	$('#new-quote').on('touchstart click', function(e){
+		e.preventDefault();
+		var url = 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=';
+		$quoteBox = $('.js-quoteBox');
+		// Give the paragraph for quotes a class name to be precise
+		// I chose to give it .js-quoteBox
+		// Hide it in the DOM (You should do this in CSS instead)
+		$quoteBox.hide();
+		// Make the request
+		$.ajax({
+			url: url,
+			method: 'get',
+			cache: false
+		}).done(function(res){
+			// Use response here to modify the DOM
+			var post = res.shift();
+			$quoteBox.html(post.content);
+			// Now you can fade the js-quoteBox back into the DOM
+			$quoteBox.fadeToggle('slow', function(){
+				$(this).html(post.content);
+				// Show source if available
+				if(post.custom_meta && post.custom_meta.Source !== undefined){
+					$('#quote-source').html('Source: ' + post.custom_meta.Source);
+				} else {
+					$('#quote-source').text('');
+				}
+			}
+		);
 
-     });
-   });
-     $('#tweet-quote').on('click', function(e) {
-   		//We tell our browser not to follow that link
-   		e.preventDefault();
-   		//We get the URL of the link
-   		window.open('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + post);
-    });
-  });
+	});
+});
+$('#tweet-quote').on('click', function(e) {
+	//We tell our browser not to follow that link
+	e.preventDefault();
+	//We get the URL of the link
+	window.open('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + post);
+});
+});

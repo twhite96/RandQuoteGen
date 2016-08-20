7c0c2a7b8f39fe357e921cf552434a9e744e65a2
Tiffany White <tiffanywhitedev412@gmail.com>
2016-08-20 19:25:28 -0400

    fixed sources. Twitter has bugs.

--- js/app.js
+++ js/app.js
@@ -1,12 +1,15 @@
+var quoteBox = '';
+
 $(document).ready(function() {
 	$('#new-quote').on('touchstart click', function(e){
 		e.preventDefault();
 		var url = 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=';
-		$quoteBox = $('.js-quoteBox');
+
 		// Give the paragraph for quotes a class name to be precise
+		quoteBox = $('.js-quoteBox');
 		// I chose to give it .js-quoteBox
 		// Hide it in the DOM (You should do this in CSS instead)
-		$quoteBox.hide();
+		quoteBox.hide();
 		// Make the request
 		$.ajax({
 			url: url,
@@ -15,9 +18,9 @@ $(document).ready(function() {
 		}).done(function(res){
 			// Use response here to modify the DOM
 			var post = res.shift();
-			$quoteBox.html(post.content);
+			quoteBox.html(post.content);
 			// Now you can fade the js-quoteBox back into the DOM
-			$quoteBox.fadeToggle('slow', function(){
+			quoteBox.fadeToggle('slow', function(){
 				$(this).html(post.content);
 				// Show source if available
 				if(post.custom_meta && post.custom_meta.Source !== undefined){
@@ -29,11 +32,13 @@ $(document).ready(function() {
 		);
 
 	});
-});
-$('#tweet-quote').on('click', function(e) {
-	//We tell our browser not to follow that link
-	e.preventDefault();
-	//We get the URL of the link
-	window.open('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + post);
+	$('#tweet-quote').on('click', function(e) {
+		//We tell our browser not to follow that link
+		e.preventDefault();
+		//We get the URL of the link
+		window.open('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + quoteBox);
+		console.log(quoteBox);
+	});
+
 });
 });

deb8f8ed70b7eb115afe410ec648a7b6d4c72098
Tiffany White <tiffanywhitedev412@gmail.com>
2017-07-15 23:52:58 -0400

    Changes colorr

--- js/app.js
+++ js/app.js
@@ -1,85 +1,43 @@
 var quoteBox = '';
 
 $(document).ready(function() {
-  $('#new-quote').on('touchstart click', function(e){
-    e.preventDefault();
-    var url = 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=';
+	$('#new-quote').on('touchstart click', function(e) {
+		e.preventDefault();
+		var url = 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=';
 
-    // Give the paragraph for quotes a class name to be precise
-    quoteBox = $('.js-quoteBox');
-    // I chose to give it .js-quoteBox
-    // Hide it in the DOM (You should do this in CSS instead)
-    quoteBox.hide();
-    // Make the request
-    $.ajax({
-      url: url,
-      method: 'get',
-      cache: false
-    }).done(function(res){
-      // Use response here to modify the DOM
-      var post = res.shift();
-      quoteBox.html(post.content)[0].innerText;
-      // Now you can fade the js-quoteBox back into the DOM
-      quoteBox.fadeToggle(1000, function(){
-        $(this).html(post.content);
-        // Show source if available
-        if (typeof post.custom_meta !== 'undefined' && typeof post.custom_meta.Source !== 'undefined') {
-          $('#quote-source').html('Source:' + post.custom_meta.Source);
-        } else {
-          $('#quote-source').text('');
-        }
-      });
+		// Give the paragraph for quotes a class name to be precise
+		quoteBox = $('.js-quoteBox');
+		// I chose to give it .js-quoteBox
+		// Hide it in the DOM (You should do this in CSS instead)
+		quoteBox.hide();
+		// Make the request
+		$.ajax({
+			url: url,
+			method: 'get',
+			cache: false
+		}).done(function(res) {
+			// Use response here to modify the DOM
+			var post = res.shift();
+			quoteBox.html(post.content)[0].innerText;
+			// Now you can fade the js-quoteBox back into the DOM
+			quoteBox.fadeToggle(1000, function() {
+				$(this).html(post.content);
+				// Show source if available
+				if (typeof post.custom_meta !== 'undefined' && typeof post.custom_meta.Source !== 'undefined') {
+					$('#quote-source').html('Source:' + post.custom_meta.Source);
+				} else {
+					$('#quote-source').text('');
+				}
+			})
 
-    });
-    $('#tweet-quote').on('click', function(e) {
-      //We tell our browser not to follow that link
-      e.preventDefault();
-      //We get the URL of the link
-      window.open('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + quoteBox.text());
-      console.log(quoteBox);
-    });
+		});
+		$('#tweet-quote').on('click', function(e) {
+			//We tell our browser not to follow that link
+			e.preventDefault();
+			//We get the URL of the link
+			window.open('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + quoteBox.text());
+			console.log(quoteBox);
+		});
 
-  });
-}); var quoteBox = '';
-
-$(document).ready(function() {
-  $('#new-quote').on('touchstart click', function(e) {
-    e.preventDefault();
-    var url = 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=';
-
-    // Give the paragraph for quotes a class name to be precise
-    quoteBox = $('.js-quoteBox');
-    // I chose to give it .js-quoteBox
-    // Hide it in the DOM (You should do this in CSS instead)
-    quoteBox.hide();
-    // Make the request
-    $.ajax({
-      url: url,
-      method: 'get',
-      cache: false
-    }).done(function(res) {
-      // Use response here to modify the DOM
-      var post = res.shift();
-      quoteBox.html(post.content)[0].innerText;
-      // Now you can fade the js-quoteBox back into the DOM
-      quoteBox.fadeToggle(1000, function() {
-        $(this).html(post.content);
-        // Show source if available
-        if (typeof post.custom_meta !== 'undefined' && typeof post.custom_meta.Source !== 'undefined') {
-          $('#quote-source').html('Source:' + post.custom_meta.Source);
-        } else {
-          $('#quote-source').text('');
-        }
-      })
-
-    });
-    $('#tweet-quote').on('click', function(e) {
-      //We tell our browser not to follow that link
-      e.preventDefault();
-      //We get the URL of the link
-      window.open('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + quoteBox.text());
-      console.log(quoteBox);
-    });
-
-  });
+	});
 });

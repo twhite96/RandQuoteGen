14442ba4a4056cca09b072fb7e9c30c9cb24b2ab
Tiffany White <hey@tiffanyrwhite.com>
2020-07-05 03:29:24 -0400

    New API

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ var quoteBox = '';
 $(document).ready(function() {
 	$('#new-quote').on('touchstart click', function(e) {
 		e.preventDefault();
-		var url = 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=';
+		var url = 'https://api.quotable.io/random';
 
 		// Give the paragraph for quotes a class name to be precise
 		quoteBox = $('.js-quoteBox');
@@ -11,25 +11,21 @@ $(document).ready(function() {
 		// Hide it in the DOM (You should do this in CSS instead)
 		quoteBox.hide();
 		// Make the request
-		$.ajax({
-			url: url,
-			method: 'get',
-			cache: false
-		}).done(function(res) {
+		$.getJSON(url, function(data) {
+			console.log(`${data.content} —${data.author}`)
 			// Use response here to modify the DOM
-			var post = res.shift();
-			quoteBox.html(post.content)[0].innerText;
+			quoteBox.html(data.content)[0].innerText;
 			// Now you can fade the js-quoteBox back into the DOM
 			quoteBox.fadeToggle(1000, function() {
-				$(this).html(post.content);
+				$(this).html(data.content);
 				// Show source if available
-				if (typeof post.custom_meta !== 'undefined' && typeof post.custom_meta.Source !== 'undefined') {
-					$('#quote-source').html('Source:' + post.custom_meta.Source);
+				if (typeof data.author !== 'undefined') {
+					$('#quote-source').html('— ' + data.author);
 				} else {
 					$('#quote-source').text('');
 				}
 			})
-		});
+     })
 		$('#tweet-quote').on('click', function(e) {
 			//We tell our browser not to follow that link
 			e.preventDefault();
